7839f5b97514e3d329ec0a22f3b80df843aa7e7d
SFMack <sfcamack@icloud.com>
2020-06-19 21:03:26 -0700

    sessions and cookies on login implemented

--- api/server.js
+++ api/server.js
@@ -4,10 +4,34 @@ const cors = require("cors");
 
 const server = express();
 
+const authenticate = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 const trucksRouter = require("../trucks/trucks-router.js");
 
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
+
+const sessionConfig = {
+  name: "auth",
+  secret: "super secret",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexSessionStore({
+    knex: require("../data/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sessionID",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
